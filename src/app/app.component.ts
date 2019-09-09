1329ade896773ef52ce41d3a5d1290fb19005d3d
Marc Melcher <marcdavidmelcher@gmail.com>
2019-09-09 11:17:17 -0500

    changed ICON & added changeevent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn';
+
+  checkInput(){
+    console.log(1);
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TypeComponent } from './type/type.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypeComponent
   ],
   imports: [
     BrowserModule
