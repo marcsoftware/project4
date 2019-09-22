6e41ab7b161e848c8141006a28266c2a5f9a535f
Marc Melcher <marcdavidmelcher@gmail.com>
2019-09-22 12:05:52 -0500

    routing sort of works.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TypeComponent } from './type/type.component';
 
+
+const appRoutes: Routes = [
+  { path: 'test', component: TypeComponent },
+
+];
+
+
 @NgModule({
   declarations: [
+
     AppComponent,
     TypeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
