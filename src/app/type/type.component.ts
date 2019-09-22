eb52dc523075335f810a8af233fa99a486204ca4
Marc Melcher <marcdavidmelcher@gmail.com>
2019-09-22 14:21:06 -0500

    test gerparam works with example data http://localhost:4200/animal/marc

--- src/app/type/type.component.ts
+++ src/app/type/type.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-type',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypeComponent implements OnInit {
 animal='did not do anything';
-  constructor() { }
+constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.animal = this.route.snapshot.paramMap.get("animal")
   }
 
 }
