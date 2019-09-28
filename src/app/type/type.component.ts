630ab32520b71fb46a22a2b433e6f71071200144
Marc Melcher <marcdavidmelcher@gmail.com>
2019-09-28 18:39:45 -0500

    bugfix: user has to type first translation, other translations are ignored

--- src/app/type/type.component.ts
+++ src/app/type/type.component.ts
@@ -62,7 +62,7 @@ dict;
   //check users input, and five user right/wrong feedback
   checkInput(event: any){
     let user_input=event.target.value;
-    this.feedback=this.dict[this.question]===user_input;
+    this.feedback=this.ans_key===user_input;
     if(this.feedback){
       this.correctInput(event);
     }
