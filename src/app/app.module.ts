378a4a6f8247aee0099b15b3fefdc3ab0b68cf81
Marc Melcher <marcdavidmelcher@gmail.com>
2019-10-06 17:19:20 -0500

    started hover component dummy file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TypeComponent } from './type/type.component';
 import { HomeComponent } from './home/home.component';
 import { StudyComponent } from './study/study.component';
+import { HoverComponent } from './hover/hover.component';
 
 
 
 const appRoutes: Routes = [
   { path: 'subject/:subject', component: TypeComponent },
   { path: '', component: HomeComponent },
-  { path: 'study/:subject', component: StudyComponent }
+  { path: 'study/:subject', component: StudyComponent },
+  { path: 'hover/:subject', component: HoverComponent }
 
 ];
 
@@ -23,7 +25,8 @@ const appRoutes: Routes = [
     AppComponent,
     TypeComponent,
     HomeComponent,
-    StudyComponent
+    StudyComponent,
+    HoverComponent
   ],
   imports: [
     BrowserModule,
