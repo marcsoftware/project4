d91114357ee79e54d06eb4797403d2b81d18fedd
marcsoftware <you@example.com>
2020-02-24 16:38:22 -0700

    updated angular but has error message now

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to learn!');
+    expect(page.getTitleText()).toEqual('project4 app is running!');
   });
 
   afterEach(async () => {
